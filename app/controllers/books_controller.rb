64ca734129490cc1e57f93430ac1614757945115
EC2 Default User <ec2-user@ip-172-31-29-237.ap-northeast-1.compute.internal>
2023-04-09 07:23:42 +0000

    テストクリア

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,7 +26,7 @@ class BooksController < ApplicationController
   def destroy
   book = Book.find(params[:id])
   book.destroy
-  redirect_to books_index_path
+  redirect_to books_path
   end
 
   def create
